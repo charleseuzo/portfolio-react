caa21bb2646a968c1d2f9c954bfdbad88fbb2941
charles <charles_paquet@hotmail.com>
2025-10-16 16:30:07 -0400

    creation du schema json

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Content from './Content'
+import Schema from './Schema'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Schema />
       <Content />
     </>
   )
