3f942f238341986b796c46999684dc2986321158
charles <charles_paquet@hotmail.com>
2025-11-27 15:35:11 -0500

    header et apropos

--- src/components/ui/Cards/Cards.jsx
+++ src/components/ui/Cards/Cards.jsx
@@ -1,19 +1,19 @@
 import styles from "./Cards.module.css";
 import Card from "../Card/Card";
 
-
-
 function Cards({ cardsData }) {
   return (
-    <div className={`${styles.cards} flex flex-wrap content-center justify-between items-start`}>
-        {cardsData.map((card, index, duration) => (
-            <Card
-                key={index}
-                title={card.title}
-                link={card.link}
-                duration={card.duration}
-            />
-        ))}
+    <div
+      className={`${styles.cards} flex flex-wrap content-center justify-between items-start`}
+    >
+      {cardsData.map((card, index) => (
+        <Card
+          key={card.id || index}
+          title={card.title}
+          link={card.link}
+          duration={card.duration}
+        />
+      ))}
     </div>
   );
 }
