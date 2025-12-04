4769457fbbbdae05edb3e8aca98deac382577cfa
Charles Paquet <charles_paquet@hotmail.com>
2025-12-04 17:21:19 -0500

    contact, header, footer, font, tour avec choux

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 // Pages
 import Home from "./pages/Home";
 import Projets from "./pages/Projets";
+import ProjetDetail from "./pages/ProjetDetail";
+import Contact from "./pages/Contact";
 import PolitiqueConfidentialite from "./pages/PolitiqueConfidentialite";
 import NotFound from "./pages/NotFound";
 
@@ -11,6 +13,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/projets/" element={<Projets />} />
+      <Route path="/projets/:slug" element={<ProjetDetail />} />
+      <Route path="/contact/" element={<Contact />} />
       <Route
         path="/politique-de-confidentialite/"
         element={<PolitiqueConfidentialite />}
